b594d9a166170170794da0d9a86455814175c17d
Ruben <ruben.samuelsen@tidsbanken.no>
2022-02-21 21:04:39 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Security.Cryptography;
+
+using RSA rsa = RSA.Create(2048);
+Console.WriteLine($"-----Private key-----{Environment.NewLine}{Convert.ToBase64String(rsa.ExportRSAPrivateKey())}{Environment.NewLine}");
+Console.WriteLine($"-----Public key-----{Environment.NewLine}{Convert.ToBase64String(rsa.ExportRSAPublicKey())}");
---
+++ readme.md
@@ -0,0 +1,9 @@
+Generates a rsa key with private and public key.
+Writes to console the string version of the private and public key.
+
+To run:
+dotnet restore
+dotnet run
+
+requirements:
+net6.0 sdk & runtime.
